fe5b4d5f22f0b1b3b1941cdab5f9fb3ca7e64435
tutzy <tatqna.miteva@gmail.com>
2015-06-30 18:49:26 +0300

    add DrowConsole

--- game/FeelTheField/ConsoleAplication.cs
+++ game/FeelTheField/ConsoleAplication.cs
@@ -10,7 +10,7 @@ using Logic.GameObjects;
 
 namespace FeelTheField
 {
-    class ConsoleAplication 
+    class ConsoleAplication
     {
         static void Main()
         {
@@ -18,38 +18,11 @@ namespace FeelTheField
             char ch2 = '$';
             int row = int.Parse(Console.ReadLine());
             int col = int.Parse(Console.ReadLine());
-            char[,] matrix = new char[row,col];
-            FillWithChar(matrix, ch1);
-            PrintField(matrix);
+            char[,] matrix = new char[row, col];
 
+
+            DrowConsole.FillWithChar(matrix, ch1);
+            DrowConsole.PrintField(matrix);
         }
-
-        public static void PrintField(char[,] field)
-        {
-            StringBuilder fillTheField = new StringBuilder();
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    fillTheField.Append(field[i, j]);
-                }
-                fillTheField.AppendLine();
-            }
-            Console.WriteLine(fillTheField);
-        }
-
-        public static void FillWithChar(char[,] inputChar, char addChar)
-        {
-            for (int i = 0; i < inputChar.GetLength(0); i++)
-            {
-                for (int j = 0; j < inputChar.GetLength(1); j++)
-                {
-                    inputChar[i, j] = addChar;
-                }
-            }
-        }
-
-
-
     }
 }
---
+++ game/FeelTheField/DrowConsole.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeelTheField
+{
+    class DrowConsole
+    {
+        public static void PrintField(char[,] field)
+        {
+            StringBuilder fillTheField = new StringBuilder();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    fillTheField.Append(field[i, j]);
+                }
+                fillTheField.AppendLine();
+            }
+            Console.WriteLine(fillTheField);
+        }
+
+        public static void FillWithChar(char[,] inputChar, char addChar)
+        {
+            for (int i = 0; i < inputChar.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputChar.GetLength(1); j++)
+                {
+                    inputChar[i, j] = addChar;
+                }
+            }
+        }
+
+    }
+}
